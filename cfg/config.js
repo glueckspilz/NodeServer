de170d0cea0442dca923ed77d059d89d32c986ee
Lukas Genkinger <lukas.genkinger@web.de>
2016-12-27 14:59:42 +0100

    Merge

--- cfg/config.js
+++ cfg/config.js
@@ -8,11 +8,7 @@ config.mysql = {};
 /*Definitions*/
 config.mysql.host = "localhost";
 config.mysql.user = "root";
-
 config.mysql.password = "";
-
-config.mysql.password = "{BLANK}";
-
 config.mysql.database = "SecuMod";
 
 
