17a36d0444587a7de9af63e85a4ef8996b52ef6c
Lukas Genkinger <lukas.genkinger@web.de>
2017-01-08 17:13:08 +0100

    container/css fix

--- cfg/config.js
+++ cfg/config.js
@@ -8,7 +8,7 @@ config.mysql = {};
 /*Definitions*/
 config.mysql.host = "localhost";
 config.mysql.user = "root";
-config.mysql.password = "";
+config.mysql.password = "xz1495a";
 config.mysql.database = "SecuMod";
 
 
